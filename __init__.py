58ac70f307f8a30b6ffcb6ba87b7acc22ee4e37c
JoergZ2 <joerg.zywek@gmx.de>
2025-01-22 09:37:03 +0100

    Signed-off-by: JoergZ2 <joerg.zywek@gmx.de>

---
+++ __init__.py
@@ -0,0 +1,24 @@
+from ovos_utils import classproperty
+from ovos_utils.process_utils import RuntimeRequirements
+from ovos_workshop.decorators import intent_handler
+from ovos_workshop.skills import OVOSSkill
+from ovos_bus_client.message import Message
+
+
+class WakeUpSkill(OVOSSkill):
+    @classproperty
+    def runtime_requirements(self):
+        return RuntimeRequirements(internet_before_load=False,
+                                   network_before_load=False,
+                                   gui_before_load=False,
+                                   requires_internet=False,
+                                   requires_network=False,
+                                   requires_gui=False,
+                                   no_internet_fallback=True,
+                                   no_network_fallback=True,
+                                   no_gui_fallback=True)
+
+    @intent_handler("are.you.awake.intent")
+    def handle_when_were_you_born_intent(self, message: Message):
+        self.speak_dialog("yes.im.awake")
+
